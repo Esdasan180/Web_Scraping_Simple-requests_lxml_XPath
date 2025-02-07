e111e71248f5f507e63b6d18c5fa34ca59849aa6
Esdasan180 <estebansantillansuarez@gmail.com>
2025-02-07 20:32:48 -0300

    Actualización de README, extraer_libros.py, libros.json e incorporacion de directorio imagenes

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Web Scraping Simple de Libros desde Books to Scrape (**requests**, **lxml**, **XPath**)
 
-Este script en Python realiza un **web scraping** del sitio [Books to Scrape](https://books.toscrape.com), extrayendo información relevante de los libros disponibles en el catálogo. Los datos recopilados incluyen el título del libro, la calificación en estrellas, el precio y la disponibilidad en stock. La información se guarda en un archivo `libros.json` para su posterior análisis.
+Este script en Python realiza un **web scraping** del sitio [Books to Scrape](https://books.toscrape.com), extrayendo información relevante de los libros disponibles. 
+
+## Metadatos
+- Title: título del libro
+- Stars: puntuación en estrellas
+- Price (euros): precio del libro en  euros
+- Stock: cantidad de libros disponibles
+- Category: categoría a la que pertenece el libro. ej: Travel, Mystery, Historical Fiction, etc.
+- Description: Descrición del libro
+- UPC: código único que identifica el producto en el catálogo
+- Product Type: Tipo de producto
+- Price (excl. tax): precio del producto antes de aplicar impuestos
+- Price (incl. tax): precio total del producto incluyendo impuestos
+- Tax: valor del impuesto aplicado al producto
+- Number of reviews: número de reseñas
+- Image: imagen de la tapa del libro
+- Link: enlace a la publicación del libro
+- Date: Fecha de la extracción de los datos del libro
+
+
 
 ## Requisitos
 
@@ -22,28 +41,29 @@ o tambien puedes intalar las dependencias del archivo requirements.txt
 pip install -r requirements.txt
 ```
 
-## Descripción del Script
+## Descripción del Script 
 
 1. **Solicitud de Páginas**: El script navega por todas las páginas del sitio web, comenzando desde la página 1 hasta la última página disponible en el catálogo.
-2. **Extracción de Datos**: Para cada libro encontrado, se extraen los siguientes datos:
-   - **Título** del libro.
-   - **Calificación** en estrellas (convertida a valores numéricos de 1 a 5).
-   - **Precio** del libro.
-   - **Disponibilidad** en stock.
+2. **Extracción de Datos**: Para cada libro encontrado, se extraen los siguientes que describo en los metadatos de este readme.
 3. **Guardado en JSON**: Toda la información recopilada se almacena en un archivo llamado `libros.json`.
 
 
 ## Consideraciones
 
 - Este script está diseñado con fines educativos y de práctica de **web scraping**. La misma página [Books to Scrape](https://books.toscrape.com) tiene estos fines.
-- Si bien este codigo es simple, en la posteridad realizaré lo mismo pero para un entorno big data así quede a disposición este codigo sencillo de extracción de datos y el completo.
-- En este caso usé **requests** para traer los datos pero también traeré el código simple usando **selenium** para navegar cada producto y obtener datos adicionales.
+- Si bien este codigo es simple, en la posteridad realizaré lo mismo pero para un entorno big data  y con hilos, así quede a disposición este codigo sencillo de extracción de datos y el completo.
+- Tuve que usar un time.sleep en la sección de obtención de datos de cada libro porque al parecer la página no permite tantas solicitudes en poco tiempo.
+- Resulta gracioso que tenga que usar un time.sleep(0.2) y el script se ejecute en 1046 segundos aproximadamente (como muestro en la imagen) y con selenium siguiendo la misma estructura de scraping pero en luhar de un time.sleep uso WebDriverWait tarde casi la mitad de tiempo siendo 676 segundos aproximadamente [adjunto aquí el enlace al script con selenium](https://github.com/Esdasan180/Web_Scraping_Simple-Selenium_XPath_WebDriverWait). Es decir que hay cosas por mejorar en este script que será modificado en cuanto sea solucionado porque resulta raro que usando selenium puedo ahorrar tiempo de ejecusión.
+
+### script con request
+![imagen con WebDriverWait](imagenes/consumo_de_script_con_requests.PNG)
+
 
 ## Problemas Comunes
 
 - **Error de conexión**: Si el sitio está caído o hay problemas de red, el script mostrará un mensaje de error.
 - **Cambios en la estructura del sitio**: Si la estructura del HTML cambia, los XPath utilizados pueden dejar de funcionar correctamente.
 
-## Autor
+## Objetivo
 
 Este script fue creado para practicar técnicas de **web scraping** utilizando **requests** y **lxml** para aplicar **XPath**.
--- extraer_libros.py
+++ extraer_libros.py
@@ -1,65 +1,178 @@
-import requests
-from lxml import html
+import time
 import json
+import requests
+import itertools
+from lxml import html
+import psutil #opcional para medir el rendimiento del script
+import time
+from datetime import date
 
 
+#mido rendimiento del script
+inicio = time.time()
+proceso = psutil.Process()
 
-numero_pagina=1
-lista_libros = []
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+} #porque hay sitios que no aceptan bots
+
+
+"""_________________________________________________________________________________________________
+____________________________________________________________________________________________________
+_________________________________OBTENGO LOS ENLACES DE CADA PAGINA_________________________________
+____________________________________________________________________________________________________
+_________________________________________________________________________________________________
+                                                                                                """
+
+
+numero_pagina=1 #para poder cambiar de página
 pagina_final = 3 #solo es para darle un valor para realizar la primera iteración entre páginas luego este valor se modifica al valor real
 
+lista_de_listas=[]
 
 while numero_pagina <= pagina_final:
-  url = f'https://books.toscrape.com/catalogue/page-{numero_pagina}.html'
-  response = requests.get(url)
-  
+    url = f'https://books.toscrape.com/catalogue/page-{numero_pagina}.html'
+    response = requests.get(url, headers=HEADERS, timeout=10)
 
-  if response.status_code == 200:
-    tree = html.fromstring(response.content)
-    pagina_final =  int(tree.xpath('//div/ul[contains(@class, "pager")]/li[contains(@class, "current")]/text()')[0].split('of ')[1].split('\n')[0])
-    libros = tree.xpath('//article')
+    if response.status_code == 200:
+        tree = html.fromstring(response.content)
+        total_libros = int(tree.xpath( '//strong')[0].text)
+        pagina_final =  int(tree.xpath('//div/ul[contains(@class, "pager")]/li[contains(@class, "current")]/text()')[0].split('of ')[1].split('\n')[0])
+        
+        lista_enlaces_porpagina = tree.xpath('//article/h3/a/@href')
+        lista_de_listas.append(lista_enlaces_porpagina)
+        numero_pagina += 1
 
-    for libro in libros:
-      #Obtengo los datos que me interesan
-      estrellas = libro.xpath('p/@class')[0].split(" ")[1].lower()
-      
-      titulo = libro.xpath('h3/a/@title')[0]
-      
-      precio = libro.xpath('div[contains(@class, "price")]/p[contains(@class, "price")]/text()')[0]
-      
-      stock = libro.xpath('div[contains(@class, "price")]/p[contains(@class, "stock")]/i/@class')[0].split("-")[1]
-      
-      #Transformo los valores de estrellas a enteros
-      estrellas_dicc = {
-          "one" : 1,
-          "two" : 2,
-          "three" : 3,
-          "four" : 4,
-          "five" : 5
-      }
 
-      estrella = estrellas_dicc.get(estrellas, 0)
 
-      #creo el diicionario con los datos
-      libro_info = {
-          "Title" : titulo,
-          "Stars" : estrella,
-          "Price" : precio,
-          "Stock" : stock
-      }
-      #incorporo los datos del libro a la lista
-      lista_libros.append(libro_info)
+
+lista_enlaces_conmpleta = list(itertools.chain(*lista_de_listas))
+
+
+"""_________________________________________________________________________________________________
+____________________________________________________________________________________________________
+__________________________________OBTENGO LOS DATOS DE CADA LIBRO___________________________________
+____________________________________________________________________________________________________
+_________________________________________________________________________________________________
+                                                                                                """
+
+
+lista_libros=[]
+
+for libro in lista_enlaces_conmpleta:
+    url_libro = f'https://books.toscrape.com/catalogue/{libro}'
+    busqueda_libro= requests.get(url_libro, headers=HEADERS, timeout=10)
+    if busqueda_libro.status_code == 200:
+        tree = html.fromstring(busqueda_libro.content)
+        try:
+            titulo = tree.xpath('//h1/text()')[0]
+        except:
+            titulo = None
     
-
+        try:
+            categoria = tree.xpath('//ul[contains(@class, "breadcrumb")]/li[3]/a/text()')[0]
+        except:
+            categoria = None
     
-  elif response.status_code != 200:
-    print("Error al encontrar la página")
-  numero_pagina += 1
+        try:
+            precio = float(tree.xpath('//p[contains(@class, "price_color")]/text()')[0].replace('£', ''))
+        except:
+            precio = None
+    
+        try:
+            disponibles = int(tree.xpath('//p[contains(@class, "instock")]/text()')[1].strip().split("(")[1].split()[0])
+        except:
+            disponibles = None
+    
+        try:
+            estrellas = tree.xpath('//p[contains(@class, "star-rating")]/@class')[0].split()[-1].lower()
+            estrellas_dicc = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
+            estrellas = estrellas_dicc.get(estrellas, 0)
+        except:
+            estrellas = None
+    
+        try:
+            descripcion = tree.xpath('//article/p/text()')
+            descripcion = descripcion[0] if descripcion else None
+        except:
+            descripcion = None
+    
+        try:
+            upc = tree.xpath('//table//th[text()="UPC"]/following-sibling::td/text()')[0]
+        except:
+            upc = None
+    
+        try:
+            price_excl_tax = float(tree.xpath('//table//th[text()="Price (excl. tax)"]/following-sibling::td/text()')[0].replace('£', ''))
+        except:
+            price_excl_tax = None
+    
+        try:
+            price_incl_tax = float(tree.xpath('//table//th[text()="Price (incl. tax)"]/following-sibling::td/text()')[0].replace('£', ''))
+        except:
+            price_incl_tax = None
+    
+        try:
+            tax = float(tree.xpath('//table//th[text()="Tax"]/following-sibling::td/text()')[0].replace('£', ''))
+        except:
+            tax = None
+    
+        try:
+            resenas = int(tree.xpath('//table//th[text()="Number of reviews"]/following-sibling::td/text()')[0])
+        except:
+            resenas = None
+    
+        try:
+            imagen = tree.xpath('//div[contains(@id, "gallery")]/div/div/div/img/@src')[0]
+            imagen = "https://books.toscrape.com" + imagen.replace("../", "/")
+        except:
+            imagen = None
+    
+        hoy = date.today().strftime("%Y-%m-%d")
 
-#En caso de haber obtenido datos de aunque sea 1 libro, creo el json
+        libro_info = {
+            "Title": titulo,
+            "Stars": estrellas,
+            "Price (euros)": precio,
+            "Stock": disponibles,
+            "Category": categoria,
+            "Description": descripcion,
+            "UPC": upc,
+            "Price (excl. tax)": price_excl_tax,
+            "Price (incl. tax)": price_incl_tax,
+            "Tax": tax,
+            "Number of reviews": resenas,
+            "Image": imagen,
+            "Link": url_libro,
+            "Date": hoy
+        }
+        lista_libros.append(libro_info)
+        time.sleep(0.2)
+
+
+"""_________________________________________________________________________________________________
+____________________________________________________________________________________________________
+_____________________________________CREO EL JSON CON LOS DATOS_____________________________________
+____________________________________________________________________________________________________
+_________________________________________________________________________________________________
+                                                                                                """
+
+
+#En caso de haber obtenido datos de aunque sea 1 libro, creo el archivo json con los datos
 if len(lista_libros) != 0:
-  with open('libros.json', 'w', encoding='utf-8') as f:
-      json.dump(lista_libros, f, ensure_ascii=False, indent=4)
-  print("Obtención de datos finalizada")
+    with open('libros.json', 'w', encoding='utf-8') as f:
+        json.dump(lista_libros, f, ensure_ascii=False, indent=4)
+    print(f"Obtención de datos finalizada. {len(lista_libros)} libros procesados de {total_libros} disponibles.")
 else:
-  print("No se obtuvo datos de ningun libro")
+    print("No se obtuvo datos de ningun libro")
+
+
+
+#mido rendimiento del script
+fin = time.time()
+uso_memoria = proceso.memory_info().rss / (1024 * 1024)  # Convertir a MB
+uso_cpu = proceso.cpu_percent(interval=1)
+
+print(f"Tiempo: {fin - inicio:.4f} segundos")
+print(f"Memoria usada: {uso_memoria:.2f} MB")
+print(f"Uso de CPU: {uso_cpu:.2f}%")
